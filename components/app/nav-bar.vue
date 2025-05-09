8bc62fea138317e30803949b2aa0f890628b14dc
a.soltani <soltaniali.1994@gmail.com>
2025-05-09 15:41:08 +0330

    add hero and navbar

---
+++ components/app/nav-bar.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="navbar bg-neutral text-neutral-content">
+    <div class="navbar-start">
+      <NuxtLink class="btn btn-ghost text-xl" to="/">
+        Location Log
+      </NuxtLink>
+    </div>
+    <div class="navbar-end">
+      <button class="btn btn-accent">
+        sign in with github
+        <Icon name="tabler:brand-github" size="24" />
+      </button>
+    </div>
+  </div>
+</template>
---
+++ layouts/default.vue
@@ -0,0 +1,12 @@
+<script>
+
+</script>
+
+<template>
+  <div class="flex min-h-screen flex-col">
+    <AppNavBar />
+    <main>
+      <slot />
+    </main>
+  </div>
+</template>
