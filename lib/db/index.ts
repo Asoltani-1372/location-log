373b991fc83f1aa68372bb8b59ed3cc4377b7bbe
a.soltani <soltaniali.1994@gmail.com>
2025-05-12 21:40:37 +0330

    add better auth and auth schema

--- lib/db/index.ts
+++ lib/db/index.ts
@@ -4,9 +4,11 @@ import { drizzle } from 'drizzle-orm/better-sqlite3'
 import * as schema from './schema'
 
 const sqlite = new Database('./lib/db/sqlite.db')
-export const db = drizzle(sqlite, { schema })
+const db = drizzle(sqlite, { schema })
 
 // Ensure the database is properly closed when the application shuts down
 process.on('exit', () => {
   sqlite.close()
 })
+
+export default db
