e12e20341ada727b2c6179b4f010adfe62362701
a.soltani <soltaniali.1994@gmail.com>
2025-05-16 13:26:01 +0330

    [feat] #21 - add sidebar

--- pages/dashboard/index.vue
+++ pages/dashboard/index.vue
@@ -1,5 +1,28 @@
+<script lang="ts" setup>
+const isSidebarOpen = ref(true)
+function toogleSidebar() {
+  isSidebarOpen.value = !isSidebarOpen.value
+  localStorage.setItem('isSidebarOpen', isSidebarOpen.value.toString())
+}
+onMounted(() => {
+  isSidebarOpen.value = localStorage.getItem('isSidebarOpen') === 'true'
+})
+</script>
+
 <template>
-  <h1>
-    u are loged in
-  </h1>
+  <div class="flex-1 flex">
+    <div class="bg-base-100 transition-all duration-300" :class="{ 'w-64': isSidebarOpen, 'w-16': !isSidebarOpen }">
+      <div class="flex hover:cursor-poiner hover:bg-base-200 p-2" :class="{ 'justify-center': !isSidebarOpen, 'justify-end': isSidebarOpen }" @click="toogleSidebar">
+        <Icon v-if="isSidebarOpen" name="tabler:chevron-left" size="32" />
+        <Icon v-else name="tabler:chevron-right" size="32" />
+      </div>
+      <div class="flex flex-col ">
+        <SiderbarBtn :show-label="isSidebarOpen" icon="tabler:map" label="location" href="/dashboard/" />
+        <SiderbarBtn :show-label="isSidebarOpen" icon="tabler:circle-plus-filled" label="Add Locations" href="/dashboard/add/" />
+        <div class="divider" />
+        <SiderbarBtn :show-label="isSidebarOpen" icon="tabler:logout-2" label="Sign Out" href="/sign-out/" />
+      </div>
+    </div>
+    <div class="flex-1" />
+  </div>
 </template>
