bace803e0fefc620f0d4b000dad005b8fb2a0958
a.soltani <soltaniali.1994@gmail.com>
2025-06-02 23:17:43 +0330

    add delete button for location page

--- pages/dashboard/location/[slug].vue
+++ pages/dashboard/location/[slug].vue
@@ -25,7 +25,28 @@ onBeforeRouteUpdate((to) => {
       </h2>
     </div>
     <div v-if="route.name === 'dashboard-location-slug' && currentLocation && currentLocationStatus !== 'pending'">
-      <h2>{{ currentLocation?.name }}</h2>
+      <div class="flex ">
+        <h2>{{ currentLocation?.name }}</h2>
+        <div class="dropdown dropdown-bottom">
+          <div tabindex="0" role="button" class="btn btn-sm mb-2 p-0">
+            <Icon size="20" name="tabler:dots-vertical" />
+          </div>
+          <ul tabindex="0" class="dropdown-content menu bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm">
+            <li>
+              <NuxtLink>
+                <Icon name="tabler:trash-x-filled" size="20" />
+                Delete
+              </NuxtLink>
+            </li>
+            <li>
+              <NuxtLink :to="{ name: 'dashboard-location-slug-edit', params: { slug: route.params.slug } }">
+                <Icon name="tabler:map-pin-cog" size="20" />Edit
+              </NuxtLink>
+            </li>
+          </ul>
+        </div>
+      </div>
+
       <p class="text-sm">
         {{ currentLocation?.description }}
       </p>
